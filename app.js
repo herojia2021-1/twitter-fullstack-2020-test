9251c4b465175bced95420209240684d968c5cc6
herojia <hodalaplus@gmail.com>
2021-11-28 09:35:58 +0800

    again

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const helpers = require("./_helpers")//加回去給你
+const helpers = require("./_helpers") //加回去給你
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated() tests test
+// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated() tests test test
 
 app.get("/", (req, res) => res.send("Hello World!"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
